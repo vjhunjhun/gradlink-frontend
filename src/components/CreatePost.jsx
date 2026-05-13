d52a2c5c393f0568bc744fc2dc561291111e01ce
Vaibhav Jhunjhunuwala <158839113+vjhunjhun@users.noreply.github.com>
2026-05-13 20:58:47 +0545

    Update API URL for adding posts

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = ({ open, setOpen }) => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8000/api/v1/post/addpost",
+        `${API_URL}/api/v1/post/addpost`,
         formData,
         {
           headers: {
